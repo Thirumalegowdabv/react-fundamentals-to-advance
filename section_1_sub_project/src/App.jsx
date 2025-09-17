d2143653716a97a8ff5c09a97205264809107ddb
Your Name <your-email@example.com>
2025-09-17 09:59:59 +0530

    Add section_1_sub_project

---
+++ section_1_sub_project/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Main from "./components/Main.jsx"
+import Navbar from './components/Navbar.jsx'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  )
+}
+
+export default App
